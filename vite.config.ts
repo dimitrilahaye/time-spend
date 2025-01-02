4474a4db1ffc4684d8751ff9f59d6a0dd0e38198
Dimitri Lahaye <dimitri.lahaye@shopopop.com>
2025-01-02 19:28:11 +0100

    chore(init): app bootstraping
    * initialize vite pwa
    * add deps
    * add github actions workflows

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [VitePWA({
+    strategies: 'injectManifest',
+    srcDir: 'src/service-worker',
+    filename: 'sw.ts',
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'time-$pend',
+      short_name: 'time-$pend',
+      description: 'L\'app qui vous rappelle que le temps c\'est de l\'argent',
+      theme_color: '#000000',
+    },
+
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
