c9c6c764cfc632b760bf1bc2fff03038ae130a6a
Dimitri Lahaye <dimitri.lahaye@shopopop.com>
2025-01-03 13:45:07 +0100

    refactor(ui): encapsule dom logic

--- src/ui/elements.ts
+++ src/ui/elements.ts
@@ -3,7 +3,15 @@ function getErrorMessage() {
   if (errorMessage === null) {
     throw new Error("Error message not found");
   }
-  return errorMessage;
+  return {
+    displayMessage: (message: string) => {
+      const body = errorMessage.querySelector(".message-body");
+      if (body) {
+        body.innerHTML = message;
+      }
+      errorMessage.classList.toggle("is-hidden");
+    },
+  };
 }
 
 function getApp() {
@@ -12,16 +20,102 @@ function getApp() {
     throw new Error("app not found");
   }
 
-  return app;
+  return {
+    element: app,
+    initDom: () => {
+      app.insertAdjacentHTML(
+        "afterbegin",
+        `
+        <main class="section">
+          <header>
+            <nav class="navbar">
+              <div class="navbar-brand">
+                <span class="navbar-item has-text-weight-bold">
+                  🕰️ time | $pend
+                </span>
+              </div>
+            </nav>
+          </header>
+          <div class="container">
+            <div class="columns is-mobile is-multiline">
+              <div class="column is-full">
+                <form id="init">
+                  <div class="field is-horizontal">
+                    <div class="field-label is-normal">
+                      <label class="label has-text-grey-light" for="hourlyCost">Taux horaire</label>
+                    </div>
+                    <div class="field-body">
+                      <div class="field is-narrow has-addons">
+                        <div class="control">
+                          <input
+                            class="input"
+                            type="number" 
+                            id="hourlyCost" 
+                            name="hourlyCost" 
+                            step="0.01" 
+                            min="0" 
+                            placeholder="ex. 15.35" 
+                            required
+                          />
+                        </div>
+                        <div class="control">
+                          <a class="button is-static">
+                            €
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <button class="button is-fullwidth" type="submit">Lancer le décompte</button>
+                </form>
+                <div id="timer-container" class="is-hidden has-text-grey-light">
+                  <div class="timer-display">
+                  <div id="clock">00:00:00</div>
+                  <div class="separator"></div>
+                  <div id="money" class="has-text-weight-bold">0.00 €</div>
+                  </div>
+                  <div class="timer-actions">
+                    <button class="controls button button--pause" type="button"></button>
+                    <button class="stop button" type="button">Faire un autre décompte</button>
+                  </div>
+                </div>
+              </div>
+              <div id="error-message" class="message is-danger is-hidden">
+                <div class="message-body"></div>
+              </div>
+            </div>
+          </div>
+          <div
+            id="pwa-toast"
+            role="alert"
+            aria-labelledby="toast-message"
+          >
+            <div class="message">
+              <span id="toast-message"></span>
+            </div>
+            <div class="buttons">
+                <button id="pwa-refresh" type="button">
+                  Reload
+                </button>
+                <button id="pwa-close" type="button">
+                  Close
+                </button>
+            </div>
+          </div>
+        </main>
+    `
+      );
+    },
+  };
 }
 
-function getAmountPerHourInput() {
+function getAmountPerHourValue() {
   const amountPerHourInput = document.querySelector("#hourlyCost");
   if (amountPerHourInput === null) {
     throw new Error("Input for hourly cost not found");
   }
 
-  return amountPerHourInput;
+  return (amountPerHourInput as HTMLInputElement).value;
 }
 
 function getTimerDisplay() {
@@ -30,7 +124,10 @@ function getTimerDisplay() {
     throw new Error("Timer container not found");
   }
 
-  return timerDisplay;
+  return {
+    pause: () => timerDisplay.classList.add("timer-display--pause"),
+    play: () => timerDisplay.classList.remove("timer-display--pause"),
+  };
 }
 
 function getControlButton() {
@@ -39,7 +136,21 @@ function getControlButton() {
     throw new Error("Control button not found");
   }
 
-  return controlButton;
+  return {
+    pause: () => {
+      controlButton.classList.remove("button--pause");
+      controlButton.classList.add("button--play");
+    },
+    play: () => {
+      controlButton.classList.remove("button--play");
+      controlButton.classList.add("button--pause");
+    },
+    isPaused: () => controlButton.classList.contains("button--pause"),
+    isPlayed: () => controlButton.classList.contains("button--play"),
+    clickHandler: (handler: () => void) => {
+      controlButton.addEventListener("click", handler);
+    },
+  };
 }
 
 function getStop() {
@@ -48,7 +159,11 @@ function getStop() {
     throw new Error("Stop button not found");
   }
 
-  return stop;
+  return {
+    clickHandler: (handler: () => void) => {
+      stop.addEventListener("click", handler);
+    },
+  };
 }
 
 function getTimerContainer() {
@@ -57,7 +172,9 @@ function getTimerContainer() {
     throw new Error("Timer not found");
   }
 
-  return timerContainer;
+  return {
+    show: () => timerContainer.classList.remove("is-hidden"),
+  };
 }
 
 function getFormInit() {
@@ -66,13 +183,18 @@ function getFormInit() {
     throw new Error("Form for initialization not found");
   }
 
-  return formInit;
+  return {
+    hide: () => formInit.classList.add("is-hidden"),
+    submitHandler: (handler: (e: Event) => void) => {
+      formInit.addEventListener("submit", handler);
+    },
+  };
 }
 
 export {
   getErrorMessage,
   getApp,
-  getAmountPerHourInput,
+  getAmountPerHourValue,
   getTimerDisplay,
   getControlButton,
   getStop,
