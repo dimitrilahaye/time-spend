a5525fd36658a5d37353ade8c320a2949ed1b6fb
Dimitri Lahaye <dimitri.lahaye@shopopop.com>
2025-01-03 03:45:13 +0100

    chore(pwa): configure PWA with images

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -8,5 +8,5 @@ export default defineConfig({
         preset: '2023',
     },
     preset,
-    images: ['public/favicon.svg'],
+    images: ['public/favicon-32x32.png'],
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,75 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VitePWA({
-    strategies: 'injectManifest',
-    srcDir: 'src/service-worker',
-    filename: 'sw.ts',
-    registerType: 'autoUpdate',
-    injectRegister: false,
+  plugins: [
+    VitePWA({
+      strategies: "injectManifest",
+      srcDir: "src/service-worker",
+      filename: "sw.ts",
+      registerType: "autoUpdate",
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'time-$pend',
-      short_name: 'time-$pend',
-      description: 'L\'app qui vous rappelle que le temps c\'est de l\'argent',
-      theme_color: '#000000',
-    },
+      manifest: {
+        theme_color: "#000000",
+        background_color: "#ffffff",
+        icons: [
+          {
+            purpose: "maskable",
+            sizes: "512x512",
+            src: "icon512_maskable.png",
+            type: "image/png",
+          },
+          {
+            purpose: "any",
+            sizes: "512x512",
+            src: "icon512_rounded.png",
+            type: "image/png",
+          },
+        ],
+        screenshots: [
+          {
+            src: "/screenshots/1.png",
+            sizes: "1290x2796",
+            type: "image/png",
+          },
+          {
+            src: "/screenshots/2.png",
+            sizes: "1290x2796",
+            type: "image/png",
+          },
+          {
+            src: "/screenshots/3.png",
+            sizes: "1290x2796",
+            type: "image/png",
+          },
+        ],
+        orientation: "portrait",
+        display: "standalone",
+        lang: "fr",
+        name: "time | $pend",
+        short_name: "time | $pend",
+        start_url: "/",
+        scope: "/",
+        description: "L'app qui vous rappelle que le temps c'est de l'argent",
+      },
 
-    injectManifest: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-    },
+      injectManifest: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
-})
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
